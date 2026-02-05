089a62c1330ff1d6922db749a22f265d187c93f8
Ariel <ariel.wtf.7@gmail.com>
2026-02-05 15:53:35 -0600

    Se creó la función orquestadora para el clustering de producción y el csv con los clusters

--- src/Clustering_clientes.py
+++ src/Clustering_clientes.py
@@ -12,11 +12,11 @@ import mlflow.sklearn
 BASE_DIR = Path(__file__).resolve().parents[1]
 ruta_csv = BASE_DIR / "data" / "processed" / "Features_clusters.csv"
 Data_final = BASE_DIR / "data" / "processed"
-BASELINE_FILE = BASE_DIR / "baseline_northwind.csv"
+BASELINE_FILE = BASE_DIR / "data" / "processed" / "baseline_northwind.csv"
 
-def medir_drift_o_crear_baseline(df_nuevo, features, ruta_baseline):
+def medir_drift_o_crear_baseline(df_nuevo, ruta_baseline):
     
-    features = ['Monto', 'Frecuencia', 'UnidadesCompradas', 'TicketPromedio', 'DiversidadCategorias']
+    features = ['Monto', 'NumeroDeOrdenes', 'UnidadesCompradas', 'TicketPromedio', 'DiversidadCategorias']
     path_obj = Path(ruta_baseline)
 
     if not path_obj.exists():
@@ -39,4 +39,74 @@ def medir_drift_o_crear_baseline(df_nuevo, features, ruta_baseline):
             hay_drift_global = True
             print(f" DRIFT DETECTADO en variable: {col} (p={p_val:.4f})")
     
-    return hay_drift_global, 
+    return hay_drift_global, reporte_drift
+
+def preprocesar_datos(df):
+  
+    print(" Iniciando Preprocesamiento (Scaler + PCA)...")
+    
+    features = ['Monto', 'NumeroDeOrdenes', 'UnidadesCompradas', 'TicketPromedio', 'DiversidadCategorias']
+    # 1. Escalado
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(df[features])
+    
+    # 2. PCA
+    pca = PCA(n_components=3)
+    pca_data = pca.fit_transform(scaled_data)
+
+    varianza_total = pca.explained_variance_ratio_.sum()
+
+    mlflow.log_metric("Varianza explicada total PCA", varianza_total)
+    
+    return pca_data, scaler, pca
+
+def main(df):
+    """Función Orquestadora Principal"""
+    
+    # Iniciamos el experimento en MLflow
+    mlflow.set_experiment("Northwind_Clustering_Segmentation")
+    
+    with mlflow.start_run(run_name="DBSCAN_Produccion"):
+        
+        # 1. MONITOREO DE DRIFT
+        # Pasamos la ruta definida con pathlib
+        drift_alert, reporte = medir_drift_o_crear_baseline(df, BASELINE_FILE)
+        mlflow.log_param("drift_detected", drift_alert)
+        
+        if drift_alert:
+            print(" ALERTA: La distribución de los datos ha cambiado significativamente.")
+        
+        # 2. PREPROCESAMIENTO
+        pca_data, scaler, pca = preprocesar_datos(df)
+        
+        # 3. MODELADO (DBSCAN)
+        print(" Entrenando DBSCAN con eps=0.5 y min_samples=4")
+        dbscan = DBSCAN(eps=0.5, min_samples=4)
+        clusters = dbscan.fit_predict(pca_data)
+        
+        # 4. MÉTRICAS Y LOGS
+        n_clusters = len(set(clusters)) - (1 if -1 in clusters else 0)
+        n_noise = list(clusters).count(-1)
+        
+        if n_clusters > 1:
+            sil_score = silhouette_score(pca_data, clusters)
+            mlflow.log_metric("silhouette_score", sil_score)
+            print(f" Silhouette Score: {sil_score:.4f}")
+        
+        mlflow.log_param("epsilon ", 0.5)
+        mlflow.log_param("Min_samples ", 4)
+        mlflow.log_metric("n_clusters", n_clusters)
+        mlflow.log_metric("noise_points", n_noise)
+        
+        # 5. GUARDADO DE RESULTADOS
+        df['Cluster'] = clusters
+        output_file = Data_final / "Northwind_Clustered.csv"
+        df.to_csv(output_file, index=False)
+        
+        mlflow.sklearn.log_model(dbscan, "model_dbscan")
+
+if __name__ == "__main__":
+    df = pd.read_csv(ruta_csv)
+    main(df)
+
+
